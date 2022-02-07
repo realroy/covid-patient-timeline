5210df5653807c7160d3ee7d5e2873bce968e62c
realroy <roiboon.cc@gmail.com>
2022-02-07 09:24:15 +0700

    feat: add frontend

---
+++ modules/frontend/components/PatientForm.tsx
@@ -0,0 +1,71 @@
+import { FC, MouseEventHandler, useEffect } from "react";
+import { useForm } from "react-hook-form";
+
+export type PatientFormData = {
+  gender: string;
+  age: number;
+  occupation: string;
+};
+
+export type PatientFormProps = {
+  patientId: string;
+  onSubmit: (data: PatientFormData) => any;
+  onRemoveClick: MouseEventHandler;
+} & PatientFormData;
+
+export const PatientForm: FC<PatientFormProps> = (props) => {
+  const { gender, occupation, age, patientId } = props
+  const {
+    register,
+    handleSubmit,
+    watch,
+    setValue,
+    formState: { errors },
+  } = useForm();
+
+  useEffect(() => {
+    setValue('gender', gender)
+    setValue('occupation', occupation)
+    setValue('age', age)
+  }, [patientId, gender, occupation, age, patientId, setValue])
+  
+  return (
+    <div>
+      <div>
+        <div>Patient Information</div>
+        <button data-patient-id={props.patientId} onClick={props.onRemoveClick}>
+          Remove Patient
+        </button>
+      </div>
+      <form onSubmit={handleSubmit(props.onSubmit)}>
+        <span>
+          <label>Gender</label>
+
+          <div>
+            <select id="gender" {...register("gender")}>
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+              <option value="prefer-not-to-say">Prefer not to say</option>
+            </select>
+          </div>
+        </span>
+        <span>
+          <label>Age</label>
+
+          <div>
+            <input type="number" id="age" {...register("age")} />
+          </div>
+        </span>
+        <span>
+          <label htmlFor="occupation">Occupation</label>
+
+          <div>
+            <input type="text" id="occupation" {...register("occupation")} />
+          </div>
+        </span>
+
+        <input type="submit" value="Update" />
+      </form>
+    </div>
+  );
+};
---
+++ modules/frontend/pages/_app.tsx
@@ -0,0 +1,25 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:3000/graphql",
+  cache: new InMemoryCache(),
+});
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
+}
+
+export default MyApp;
